6a4425c4e19f515f889576dac6137d5c710fceb8
Brian Glusman <bglusman@cobroventures.com>
2014-09-08 12:33:45 -0400

    REDIS_URL env param

--- app/models/persistent_queue.rb
+++ app/models/persistent_queue.rb
@@ -44,8 +44,8 @@ class PersistentQueue
 
   def redis
     @redis ||= begin
-      if ENV["REDISCLOUD_URL"] && !$redis
-        uri = URI.parse(ENV["REDISCLOUD_URL"])
+      if ENV["REDIS_URL"] && !$redis
+        uri = URI.parse(ENV["REDIS_URL"])
         $redis = Redis.new(url: uri)
       end
       $redis || Redis.new #default port, etc, can pass in options to new if needed
